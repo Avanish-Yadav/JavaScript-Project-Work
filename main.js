3e5734271dd57e22c179eae65e86e0e207b93893
Avanish-Yadav <avanishyadav1906@gmail.com>
2024-03-11 14:40:51 +0530

    changed

---
+++ main.js
@@ -0,0 +1,20 @@
+function createReactangle(length,breadth){
+const rectangle={
+    length,
+    breadth,
+
+    draw:function(){
+        console.log("Drawing reactangle");
+    },
+    
+    area:function(){
+        console.log(`Area of Rectangle: ${rectangle.length*rectangle.breadth}`);
+    }
+};
+return rectangle;
+}
+
+let rectangle1=createReactangle(5,6);
+console.log(rectangle1.length);
+rectangle1.draw();
+rectangle1.area();
